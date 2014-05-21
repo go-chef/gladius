563f9d454a7380c84112be5f7a1d5ca7f310e9f2
AJ Christensen <aj@junglist.io>
2014-05-21 19:47:09 +1200

    gladius: remove ClientConfig struct, re-tab docs

--- gladius.go
+++ gladius.go
@@ -3,35 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/docopt/docopt-go"
-	"github.com/go-chef/gladius/chef"
 )
 
 // VERSION is the gladius version
 const VERSION = "0.0.1"
 
-type Config struct {
-	Client *chef.ClientConfig
-}
-
 func main() {
-	usage := `usage: gladius <action> <object> [<names>]
-	[-s|--server=<serverUri>] 
-	[-k|--key=<keyFile>]
-	[-c|--client=<clientName>]
-	[-f|--file=<configFile>]
-	[-o|--output=<formatter>] 
-	[--version] [--help] 
-	
-Actions: 
-	solve     Run solver or execute a solve on a server.
-	upload    Upload the object to the server.
- 	download  Download an object frmo the server.
-	edit      Edit an object in $EDITOR.
-	show      Output the contents of the object.
-	list      List objects.
+	usage :=
+		`
+usage: gladius <action> <object> [<names>]
+  [-s|--server=<serverUri>]
+  [-k|--key=<keyFile>]
+  [-c|--client=<clientName>]
+  [-f|--file=<configFile>]
+  [-o|--output=<formatter>]
+  [--version] [--help]
 
-Objects: 
-	cookbook, role, run_list, environment, data_bag
+Actions:
+solve     Run solver or execute a solve on a server.
+upload    Upload the object to the server.
+download  Download an object frmo the server.
+edit      Edit an object in $EDITOR.
+show      Output the contents of the object.
+list      List objects.
+
+Objects:
+cookbook, role, run_list, environment, data_bag
 
 See 'gladius help command' for more info on that command.
 `
