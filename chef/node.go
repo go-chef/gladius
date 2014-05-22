fbf6613ff33908d7bf663e1ad78549ef61f33294
Jesse Nelson <spheromak@gmail.com>
2014-05-21 21:35:27 -0700

    add ChefEnvironment Field to nativeNode type

--- chef/node.go
+++ chef/node.go
@@ -14,12 +14,13 @@ type RunList []string
 
 // NativeNode represents the native Go version of the deserialized Node type
 type nativeNode struct {
-	Name      string                 `mapstructure:"name"`
-	RunList   RunList                `mapstructure:"run_list"`
-	Automatic map[string]interface{} `mapstructure:"automatic"`
-	Normal    map[string]interface{} `mapstructure:"normal"`
-	Default   map[string]interface{} `mapstructure:"default"`
-	Override  map[string]interface{} `mapstructure:"override"`
+	Name        string                 `mapstructure:"name"`
+	RunList     RunList                `mapstructure:"run_list"`
+	Environment string                 `mapstructure:"chef_environment"`
+	Automatic   map[string]interface{} `mapstructure:"automatic"`
+	Normal      map[string]interface{} `mapstructure:"normal"`
+	Default     map[string]interface{} `mapstructure:"default"`
+	Override    map[string]interface{} `mapstructure:"override"`
 }
 
 // NewNode wraps a Node around a pointer to a Reader
